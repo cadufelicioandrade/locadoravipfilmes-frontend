00a55ebbaca7c23233a76414d6d9dea514eb0f25
cadufelicioandrade <cadufelicioandrade@gmail.com>
2022-06-30 00:31:23 -0300

    validators

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CadastroUsuarioService } from './cadastro-usuario.service';
+import { minusculoValidator } from './minusculo.validator';
 import { NovoUsuario } from './novo-usuario';
 
 @Component({
@@ -19,10 +20,10 @@ export class NovoUsuarioComponent implements OnInit {
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-      username:[''],
-      email:[''],
-      password:[''],
-      repassword:['']
+      username:['', [Validators.required, Validators.minLength(3), minusculoValidator]],
+      email:['', [Validators.required, Validators.email]],
+      password:['',[Validators.required]],
+      repassword:['', [Validators.required]]
     });
   }
 
